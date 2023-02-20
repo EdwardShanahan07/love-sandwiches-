753081f2b7648b03604c4f5d48bbcd43a596fbf0
Edward Shanahan <edwardshanahan357@gmail.com>
2023-02-20 22:26:36 +0000

    Add sales data to worksheet

--- run.py
+++ run.py
@@ -30,14 +30,15 @@ def get_sales_data():
             print("Data is Valid!")
             break
 
-        return sales_data
+    return sales_data
 
 
 def validate_data(values):
     """
-    Inside the try, converts all string values into integers
-    Raises ValueError if strings cannot be converted to integers,
-    or if there are'nt exactly six values
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     try:
         [int(value) for value in values]
@@ -53,4 +54,19 @@ def validate_data(values):
     return True
 
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with list data provided
+    """
+
+    print('Updating sales worksheet...\n')
+
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully!\n")
+
+
 data = get_sales_data()
+
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
